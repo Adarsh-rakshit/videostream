d7fd7aeb4690a52f09386e0a8be9306aad89ce5c
adarsh rakshit <adarshrakshit07@gmail.com>
2024-10-30 20:30:25 +0530

    connection for mongodb and installing express dotenv mongoose

---
+++ src/db/index.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connecDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n mongoDB connected DB_Host :: ${connectionInstance.connection.host}`);
+    } catch (MongoError) {
+        console.log("MONGODB ERROR :: ",MongoError);
+        process.emit(1);
+    }
+}
+export default connecDB;
